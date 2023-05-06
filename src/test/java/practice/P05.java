2d1a0e4d5de93abf2c225016f2768da6afc41661
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-05-06 23:28:28 +0300

    230506_2330

--- src/test/java/practice/P05.java
+++ src/test/java/practice/P05.java
@@ -1,5 +1,10 @@
 package practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class P05 {
     /*
 //  1-Driver oluşturalim
@@ -18,7 +23,10 @@ public class P05 {
 //  10-8. maddededeki Test sonucunu yazdırsın
      */
     public static void main(String[] args) {
-
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
 }
